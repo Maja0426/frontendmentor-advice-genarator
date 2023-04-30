7f3ce04b18384a2a52afe170b2ad89e4343edd2f
Maja0426 <tmsmajoros@gmail.com>
2023-04-30 14:17:56 +0200

    first commit, simple version of challenge

---
+++ main.js
@@ -0,0 +1,12 @@
+const quoute = document.querySelector('.quoute')
+const id = document.querySelector('.advice-id')
+const btn = document.querySelector('.dice')
+
+async function sendGetRequest() {
+  const response = await fetch('https://api.adviceslip.com/advice')
+  const data = await response.json()
+  id.innerText = data.slip.id
+  quoute.innerText = data.slip.advice
+}
+
+btn.addEventListener('click', sendGetRequest)
